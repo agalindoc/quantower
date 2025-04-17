56c475d777bebe926a0e0f399bcbb176af2cc9bc
Alejandro Galindo Chairez <69223009+agalindoc@users.noreply.github.com>
2025-04-17 09:11:35 -0400

    Update to show volume Delta

--- strategies/VolumeAccess/VolumeAccess.cs
+++ strategies/VolumeAccess/VolumeAccess.cs
@@ -1,5 +1,4 @@
-﻿//
-// Based on code from https://github.com/Quantower and @alexeybogdan
+//
 // Dev Alejandro Galindo Cháirez. 2021
 //
 using System;
@@ -29,7 +28,12 @@ namespace VolumeAccess
         double _bidVolume = 0.0;
         double _volume = 0.0;
         double _volumeOF = 0.0;
+        double _delta = 0.0;
         bool _dataLoaded = false;
+        DateTime _dateTimeLeft;
+        DateTime _dateTimeRight;
+        int _count = 0;
+
 
         public VolumeAccess()
             : base()
@@ -90,42 +94,84 @@ namespace VolumeAccess
         {
             List<StrategyMetric> result = base.OnGetMetrics();
 
+            //if (_dataLoaded)
+            //{
             // An example of adding custom strategy metrics:
             result.Add("Volume", _volume.ToString());
             result.Add("Current Ask volume", _askVolume.ToString());
             result.Add("Current Bid volume", _bidVolume.ToString());
             result.Add("Volume OF", _volumeOF.ToString());
+            result.Add("Count", _count.ToString());
+            result.Add("Time left:", _dateTimeLeft.ToString());
+            result.Add("Time right:", _dateTimeRight.ToString());
+            result.Add("Delta", _delta.ToString());
+            //}
+
+
 
             return result;
         }
 
-        private void Progress_StateChanged(object sender, VolumeAnalysisTaskEventArgs a)
-        {
-            if (a.CalculationState == VolumeAnalysisCalculationState.Finished)
-                _dataLoaded = true;
-        }
+        
         private void OnNewLast(Symbol symbol, Last last)
         {
             StrategyProcess(last);
             //Log("OnNewLast executed", StrategyLoggingLevel.Info);
         }
 
+        public void VolumeAnalysisData_Loaded()
+        {
+
+        }
+
+        private void Progress_StateChanged(object sender, VolumeAnalysisTaskEventArgs a)
+        {
+
+            if (a.CalculationState == VolumeAnalysisCalculationState.Finished)
+            {
+                this._dataLoaded = true;
+
+                Log("History Loaded", StrategyLoggingLevel.Info);
+            }
+
+        }
+
+
         private void OnNewQuote(Symbol symbol, Quote quote)
         {
+            Log($"State: {this.hdm.VolumeAnalysisCalculationProgress.State.ToString()}");
+
+            if (this.hdm.VolumeAnalysisCalculationProgress == null)
+                Log("Progress is null");
+
+            if (this.hdm.VolumeAnalysisCalculationProgress == null || this.hdm.VolumeAnalysisCalculationProgress.State != VolumeAnalysisCalculationState.Finished)
+                return;
+
             StrategyProcess(quote);
 
             _volume = ((HistoryItemBar)hdm[barShift]).Volume;
             _askVolume = ((HistoryItemBar)hdm[barShift]).VolumeAnalysisData.Total.SellVolume;
             _bidVolume = ((HistoryItemBar)hdm[barShift]).VolumeAnalysisData.Total.BuyVolume;
             _volumeOF = ((HistoryItemBar)hdm[barShift]).VolumeAnalysisData.Total.Volume;
+            _dateTimeLeft = ((HistoryItemBar)hdm[barShift]).VolumeAnalysisData.TimeLeft;
+            _delta = ((HistoryItemBar)hdm[barShift]).VolumeAnalysisData.Total.Delta;
+            _dateTimeRight = hdm.FromTime;
+            _count = hdm.Count;
+            if (double.IsNaN(_askVolume))
+                Log("_askVolume is NaN");
 
             //if (_dataLoaded)
             //{
+            //
+            // Execute here yor strategy based on volume data
+            //
             //    _askVolume = hdm[0, SeekOriginHistory.End].VolumeAnalysisData.Total.SellVolume;
             //    _bidVolume = hdm[0, SeekOriginHistory.End].VolumeAnalysisData.Total.BuyVolume;
             //}
 
             //Log("OnNewQuote executed", StrategyLoggingLevel.Info);
+
+
         }
 
 
